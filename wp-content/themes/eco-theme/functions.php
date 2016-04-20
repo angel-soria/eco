ad350b604e8d32f1fd4dc71f1a9f5565f22ac530
angelg4a <mas@g4all.mx>
2016-04-20 00:39:01 -0500

    Menu

--- wp-content/themes/eco-theme/functions.php
+++ wp-content/themes/eco-theme/functions.php
@@ -1,1 +1,22 @@
 <?php
+// Register style sheet.
+add_action( 'wp_enqueue_scripts', 'register_plugin_styles' );
+
+/**
+ * Register style sheet.
+ */
+function register_plugin_styles() {
+	wp_register_script( 'jquery-22', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js' );
+	wp_enqueue_script( 'jquery-22');
+	wp_register_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap' );
+	wp_register_script( 'bs-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' );
+	wp_enqueue_script( 'bs-js'); 
+	wp_register_style( 'global', get_template_directory_uri().'/eco-style/stylesheets/global.css' );
+	wp_enqueue_style( 'global' );
+}
+
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
--- wp-content/themes/eco-theme/header.php
+++ wp-content/themes/eco-theme/header.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
-</header>
-<div id="container">
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6 col-sm-4 col-lg-3 col-ms-5">
+					<img src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" alt="EcoVive D & C" class="img-responsive">
+				</div>
+				<div class="col-sm-8 hidden-xs col-lg-9 col-ms-7">
+					<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header__menu' ) );?>
+				</div>
+			</div>
+		</div>
+	</div>
