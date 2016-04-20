2484180aa8989049eb029058d982e6a49f66203f
angelg4a <mas@g4all.mx>
2016-04-20 18:37:16 -0500

    Footer v2

--- wp-content/themes/eco-theme/footer.php
+++ wp-content/themes/eco-theme/footer.php
@@ -1,4 +1,20 @@
 <footer>
+	<div class="contacto">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-ms-5 col-xs-12">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.png" alt="EcoVive D&C">
+					<address>
+						 01 443 3 24 77 70 ext. 201 <br/>
+
+						Av. Acueducto #95 Int. 301 Colonia Vasco de Quiroga, C.P. 58230 Morelia Michoacán<br/>
+
+						contacto@ecovivedyc.com
+					</address>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="pie">
 		<div class="container">
 			<div class="row">
